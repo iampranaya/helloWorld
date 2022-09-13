85dacf4707da715b180c84ba991027631798870e
PranayaSingh <iampranaya@hotmail.com>
2022-09-12 21:43:33 -0500

    first commit

---
+++ src/com/main/Runner.java
@@ -0,0 +1,29 @@
+package com.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int j = 1;
+		for(int i = 0; i<5; i++) {
+			System.out.println(j++);
+		}
+		
+		
+		String name= "sam";
+		switch(name) {
+		case "ram":
+			System.out.println("this is ram");
+			break;
+		case "pran":
+			System.out.println("this is pran");
+			break;
+		case "sam":
+			System.out.println("this is sam");
+			break;
+		}
+		
+	}
+
+}
